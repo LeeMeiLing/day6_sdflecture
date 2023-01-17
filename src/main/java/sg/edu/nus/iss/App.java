0993e4c30564a0beed87126f983427ad07340644
LeeMeiLing <leem0060@outlook.com>
2023-01-17 11:03:15 +0800

    Basic Threading example

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -12,6 +12,30 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        // anonymous class to implement interface
+        Thread thread1 = new Thread(new Runnable(){
+            
+            @Override
+            public void run(){
+                for (int i = 0; i< 20; i++){
+                    System.out.println(Thread.currentThread().getName() + "\t Runnable..." + i);
+                }
+            }
+        });
+
+        thread1.start();
+
+        // using class to implement interface
+        MyRunnableImplementation mRI = new MyRunnableImplementation();
+        Thread thread2 = new Thread(mRI);
+        thread2.start();
+
+        Thread thread3 = new Thread(mRI);
+        thread3.start();
+
+        // using lambda expression to implement interface
+        
+
     }
 }
