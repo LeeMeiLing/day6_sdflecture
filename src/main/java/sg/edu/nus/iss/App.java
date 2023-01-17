1d1745fe1301b0fa66c18467d2d0797ab8ec273c
LeeMeiLing <leem0060@outlook.com>
2023-01-17 11:32:02 +0800

    Threading using executor service examples

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,8 +1,9 @@
 package sg.edu.nus.iss;
 
-/**
- * Hello world!
- */
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
 public final class App {
     private App() {
     }
@@ -13,29 +14,58 @@ public final class App {
      */
     public static void main(String[] args) {
         
-        // anonymous class to implement interface
-        Thread thread1 = new Thread(new Runnable(){
+        // 1) starting thread anonymous class that implement runnable interface
+        // Thread thread1 = new Thread(new Runnable(){
             
-            @Override
-            public void run(){
-                for (int i = 0; i< 20; i++){
-                    System.out.println(Thread.currentThread().getName() + "\t Runnable..." + i);
-                }
-            }
-        });
+        //     @Override
+        //     public void run(){
+        //         for (int i = 0; i< 20; i++){
+        //             System.out.println(Thread.currentThread().getName() + "\t Runnable..." + i);
+        //         }
+        //     }
+        // });
 
-        thread1.start();
+        // thread1.start();
 
-        // using class to implement interface
-        MyRunnableImplementation mRI = new MyRunnableImplementation();
-        Thread thread2 = new Thread(mRI);
-        thread2.start();
+        // 2) starting thread using a class that implement runnable interface
+        MyRunnableImplementation mRI = new MyRunnableImplementation("task1");
+        MyRunnableImplementation mRI2 = new MyRunnableImplementation("task2");
+        MyRunnableImplementation mRI3 = new MyRunnableImplementation("task3");
+        MyRunnableImplementation mRI4 = new MyRunnableImplementation("task4");
+        MyRunnableImplementation mRI5 = new MyRunnableImplementation("task5");
 
-        Thread thread3 = new Thread(mRI);
-        thread3.start();
 
-        // using lambda expression to implement interface
-        
+        // Thread thread2 = new Thread(mRI);
+        // thread2.start();
+
+        // Thread thread3 = new Thread(mRI);
+        // thread3.start();
+
+        // 3) using single thread executor service (thread pool) to start a new thread
+        // ExecutorService executorService = Executors.newSingleThreadExecutor();
+        // executorService.execute(mRI); 
+        // executorService.execute(mRI2); // two task running on a single thread, one task complete before another
+        // executorService.shutdown(); // release resource after finishing task
+
+        // 4) using fixed thread pool to start multiple new thread
+        // ExecutorService executorService = Executors.newFixedThreadPool(3);
+        // executorService.execute(mRI); 
+        // executorService.execute(mRI2); 
+        // executorService.execute(mRI3); 
+        // executorService.execute(mRI4); // task 4 can only start after one task completed because there are only 3 threads in the thread pool
+        // executorService.execute(mRI5); // task 5 can only start after another task completed because there are only 3 threads in the thread pool
+
+        // executorService.shutdown();
+
+        // 5) using cached thread pool, max thread depend on machine #core
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.execute(mRI); 
+        executorService.execute(mRI2); 
+        executorService.execute(mRI3); 
+        executorService.execute(mRI4); 
+        executorService.execute(mRI5); 
+
+        executorService.shutdown(); 
 
     }
 }
